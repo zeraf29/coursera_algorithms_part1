42fcf7b90fc5dec9bd467ffb4a7ee96c43b547f1
zeraf29 <zeraf29@gmail.com>
2022-04-03 23:52:16 +0900

    Quick Union

---
+++ hello/lectures/union_find/dynamic_connectivity/QuickUnionUF.java
@@ -0,0 +1,30 @@
+package hello.lectures.union_find.dynamic_connectivity;
+
+public class QuickUnionUF {
+    private int[] id;
+
+    public QuickUnionUF(int N) {
+        id = new int[N];
+        //set id of each object to itself (N array accesses)
+        for (int i = 0; i < N; i++) id[i] = i;
+    }
+
+    private int root(int i) {
+        //chase parent pointers until reach root (depth of i array accesses)
+        while (i != id[i]) i = id[i];
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        //check if p and q have same root (depth of p and q array accesses)
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        //change root of p to point to root of q (depth of p and q array accesses)
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
+
+}
